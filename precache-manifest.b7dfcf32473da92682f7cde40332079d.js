ad1f438a1740b5cf664739e2af631c4edfb1c120
Inna <inna.tereshchenko1@gmail.com>
2020-01-23 18:10:38 +0200

    Updates

--- precache-manifest.b7dfcf32473da92682f7cde40332079d.js
+++ precache-manifest.b7dfcf32473da92682f7cde40332079d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e55f7fe883c8b4a8707c0f8b109845ef",
+    "revision": "55e5e9c6fe60f48357bf66ab07466cbc",
     "url": "/log_in_page/index.html"
   },
   {
-    "revision": "bc4fd36bba66fd270bcf",
-    "url": "/log_in_page/static/css/main.0facb87c.chunk.css"
+    "revision": "848d362c8d62207f9c94",
+    "url": "/log_in_page/static/css/main.5068fa49.chunk.css"
   },
   {
     "revision": "2f98801df21c3cbe35a5",
     "url": "/log_in_page/static/js/2.513c9228.chunk.js"
   },
   {
-    "revision": "bc4fd36bba66fd270bcf",
-    "url": "/log_in_page/static/js/main.9b8dfd98.chunk.js"
+    "revision": "848d362c8d62207f9c94",
+    "url": "/log_in_page/static/js/main.df822680.chunk.js"
   },
   {
     "revision": "f034f2966874915cca7d",
